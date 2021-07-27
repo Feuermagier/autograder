d989fd09b61b2e8959f4abeabd93c5e171334794
Florian Seligmann <floseligmann@web.de>
2021-07-27 10:26:56 +0200

    add tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+// https://dev.to/matebek/simplest-way-to-set-up-svelte-with-tailwind-css-41bn
+module.exports = {
+  purge: {
+    enabled: !process.env.ROLLUP_WATCH,
+    content: ['./public/index.html', './src/**/*.svelte'],
+    options: {
+      defaultExtractor: content => [
+        ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+        ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
+      ],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
