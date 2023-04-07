0beb2dbe0030aa8cb88e1d633bab834d98fe4211
Lucas <24826124+Luro02@users.noreply.github.com>
2023-04-07 12:43:24 +0200

    fix #93

--- autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/ConstantNamingAndQualifier/code/Test.java
+++ autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/ConstantNamingAndQualifier/code/Test.java
@@ -9,11 +9,13 @@ public class Test {
     private final int CONSTANT = 1; // Not ok
     private final int otherConstant = 1; // Not ok
     private final int f; // Ok, because initialized in constructor
-    
+
     private static final String weee = "foo"; // Not ok
     private final String heyHey; // Ok
     private static final Object d = null; // Ok, because not primitive or string
-    
+
+    private static final long serialVersionUID = -2338626292552177485L; // Ok, because of serialVersionUID
+
     public Test() {
         this.f = -1;
         this.heyHey = "Hey";
