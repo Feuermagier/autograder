5fbed4f632037a4bb6f2fadb766aa131a48397e2
Lucas <24826124+Luro02@users.noreply.github.com>
2023-04-08 17:13:00 +0200

    fix #45

--- autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/ListGetterSetter/code/Test.java
+++ autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/ListGetterSetter/code/Test.java
@@ -7,6 +7,8 @@ import java.util.function.Supplier;
 
 public class Test {
     private final List<Integer> list = new ArrayList<>();
+    private final int[] array = new int[10];
+    private final String[][] array2d = new String[2][2];
 
     public Test() {
         return; // Ok
@@ -17,18 +19,26 @@ public class Test {
     }
 
     public List<Integer> getList1() {
-        return this.list;
+        return this.list; // Not Ok
     }
 
     public List<Integer> getList2() {
-        return list;
+        return list; // Not Ok
     }
 
     public List<Integer> getList3() {
-        return new ArrayList<>(this.list);
+        return new ArrayList<>(this.list); // Ok
     }
 
     public List<Integer> getList4() {
-        return Collections.unmodifiableList(this.list);
+        return Collections.unmodifiableList(this.list); // Ok
+    }
+
+    public int[] getArray() {
+        return this.array;  // Not Ok
+    }
+
+    public String[][] getArray2d() {
+        return this.array2d;  // Not Ok
     }
 }
