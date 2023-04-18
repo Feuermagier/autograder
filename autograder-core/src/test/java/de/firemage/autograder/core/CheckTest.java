2c5c0a590598814440fb080bc9da9d7aba473698
Lucas <24826124+Luro02@users.noreply.github.com>
2023-04-18 08:57:13 +0200

    refactor CheckTest.java

--- autograder-core/src/test/java/de/firemage/autograder/core/CheckTest.java
+++ autograder-core/src/test/java/de/firemage/autograder/core/CheckTest.java
@@ -9,13 +9,13 @@ import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -23,55 +23,101 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class CheckTest {
     private static final boolean ENABLE_DYNAMIC = false;
 
-    @TestFactory
-    @Execution(ExecutionMode.CONCURRENT)
-    public Stream<DynamicTest> createCheckTest() throws URISyntaxException, IOException {
-        var testPath = Path.of(this.getClass().getResource("check_tests/").toURI()).toAbsolutePath();
-        return Files.list(testPath).map(path -> {
-            if (!ENABLE_DYNAMIC && Files.exists(path.resolve("tests"))) {
-                return Optional.empty();
+    private record Config(List<String> lines) {
+        public static Config fromPath(Path path) throws IOException {
+            List<String> lines = Files.readAllLines(path.resolve("config.txt"));
+
+            if (lines.size() < 2) {
+                throw new IllegalArgumentException("Config file must contain at least two lines");
             }
 
+            return new Config(lines);
+        }
+
+        public String name() {
+            return this.lines.get(1);
+        }
+
+        public List<String> expectedProblems() {
+            return new ArrayList<>(this.lines.stream()
+                    .skip(2)
+                    .filter(line -> !line.isBlank())
+                    // skip comments
+                    .filter(line -> !line.startsWith("#"))
+                    .toList());
+        }
+
+        public Check check() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+            return (Check) Class.forName("de.firemage.autograder.core.check." + this.lines.get(0)).getDeclaredConstructor().newInstance();
+        }
+    }
+
+    private record TestInput(Path path, Config config) {
+        public static TestInput fromPath(Path path) {
             try {
-                var config = Files.readAllLines(path.resolve("config.txt"));
-                return Optional.of(DynamicTest.dynamicTest("Check E2E Test: " + config.get(1), () -> {
-                    var check = (Check) Class.forName("de.firemage.autograder.core.check." + config.get(0)).getDeclaredConstructor().newInstance();
-                    var expectedProblems = new ArrayList<>(config.stream()
-                        .skip(2)
-                        .filter(line -> !line.isBlank())
-                        // skip comments
-                        .filter(line -> !line.startsWith("#"))
-                        .toList());
-
-                    var tmpDirectory = Files.createTempDirectory(null);
-
-                    var file = UploadedFile.build(path.resolve("code"), JavaVersion.JAVA_17, tmpDirectory, status -> {});
-                    var linter = new Linter(Locale.US);
-
-                    var problems =
-                        linter.checkFile(
-                            file,
-                            tmpDirectory,
-                            path.resolve("tests"),
-                            List.of(),
-                            List.of(check),
-                            status -> {
-                            }, !ENABLE_DYNAMIC || !Files.exists(path.resolve("tests")));
-
-                    for (var problem : problems) {
-                        if (!expectedProblems.remove(problem.getDisplayLocation())) {
-                            fail("The check reported a problem '" + problem.getDisplayLocation() +
-                                "' but we don't expect a problem to be there. Problem type: " + problem.getProblemType().toString() +
-                                " Message: `" + linter.translateMessage(problem.getExplanation()) + "`");
-                        }
-                    }
-                    if (!expectedProblems.isEmpty()) {
-                        fail("Problems not reported: " + expectedProblems);
-                    }
-                }));
+                return new TestInput(path, Config.fromPath(path));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }).filter(Optional::isPresent).map(o -> (DynamicTest) o.get());
+        }
+
+        /**
+         * Checks if the test is dynamic or static.
+         * @return true if the test is dynamic, false otherwise
+         */
+        public boolean isDynamic() {
+            return Files.exists(this.path.resolve("tests"));
+        }
+
+        public String testName() {
+            return "Check E2E Test: %s".formatted(this.config.name());
+        }
+    }
+
+    @TestFactory
+    @Execution(ExecutionMode.CONCURRENT)
+    Stream<DynamicTest> createCheckTest() throws URISyntaxException, IOException {
+        var testPath = Path.of(this.getClass().getResource("check_tests/").toURI()).toAbsolutePath();
+
+        List<Path> folders;
+        try (Stream<Path> paths = Files.list(testPath)) {
+            folders = paths.toList();
+        }
+
+        return DynamicTest.stream(
+            folders.stream().map(TestInput::fromPath).filter(testInput -> !testInput.isDynamic() || ENABLE_DYNAMIC),
+            TestInput::testName,
+            testInput -> {
+                var check = testInput.config().check();
+                var expectedProblems = testInput.config().expectedProblems();
+
+                var tmpDirectory = Files.createTempDirectory(null);
+
+                var file = UploadedFile.build(testInput.path().resolve("code"), JavaVersion.JAVA_17, tmpDirectory, status -> {});
+                var linter = new Linter(Locale.US);
+
+                var problems = linter.checkFile(
+                    file,
+                    tmpDirectory,
+                    testInput.path().resolve("tests"),
+                    List.of(),
+                    List.of(check),
+                    status -> {},
+                    !ENABLE_DYNAMIC || !testInput.isDynamic()
+                );
+
+                for (var problem : problems) {
+                    if (!expectedProblems.remove(problem.getDisplayLocation())) {
+                        fail("The check reported a problem '" + problem.getDisplayLocation() +
+                                "' but we don't expect a problem to be there. Problem type: " + problem.getProblemType().toString() +
+                                " Message: `" + linter.translateMessage(problem.getExplanation()) + "`");
+                    }
+                }
+
+                if (!expectedProblems.isEmpty()) {
+                    fail("Problems not reported: " + expectedProblems);
+                }
+            }
+        );
     }
 }
