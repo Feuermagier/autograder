1e9a12e348b5fdeed5b93e0c9dd37665ec2efb2a
Lucas <24826124+Luro02@users.noreply.github.com>
2023-04-22 16:31:32 +0200

    fix #113

--- autograder-core/src/main/java/de/firemage/autograder/core/check/general/FieldShouldBeFinalCheck.java
+++ autograder-core/src/main/java/de/firemage/autograder/core/check/general/FieldShouldBeFinalCheck.java
@@ -8,13 +8,10 @@ import de.firemage.autograder.core.integrated.IntegratedCheck;
 import de.firemage.autograder.core.integrated.StaticAnalysis;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtFieldWrite;
-import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtConstructor;
-import spoon.reflect.declaration.CtExecutable;
 import spoon.reflect.declaration.CtField;
 
 import java.util.Map;
-import java.util.Set;
 
 @ExecutableCheck(reportedProblems = {ProblemType.FIELD_SHOULD_BE_FINAL})
 public class FieldShouldBeFinalCheck extends IntegratedCheck {
@@ -26,27 +23,33 @@ public class FieldShouldBeFinalCheck extends IntegratedCheck {
     protected void check(StaticAnalysis staticAnalysis, DynamicAnalysis dynamicAnalysis) {
         staticAnalysis.processWith(new AbstractProcessor<CtField<?>>() {
             @Override
-            public void process(CtField<?> field) {
-                if (field.isFinal()) {
+            public void process(CtField<?> ctField) {
+                if (ctField.isFinal()) {
                     return;
                 }
 
-                Set<CtConstructor<?>> parentConstructors;
-                if (field.getDeclaringType() instanceof CtClass clazz) {
-                    parentConstructors = clazz.getConstructors();
-                } else {
-                    parentConstructors = Set.of();
-                }
-
                 boolean hasWrite = staticAnalysis.getModel()
-                    .filterChildren(c -> c instanceof CtExecutable<?> e && !parentConstructors.contains(e))
-                    .filterChildren(e -> e.filterChildren(
-                        c -> c instanceof CtFieldWrite<?> w && w.getVariable().equals(field.getReference())).first() !=
-                        null).first() != null;
-                if (!hasWrite) {
-                    addLocalProblem(field,
-                        new LocalizedMessage("field-final-exp", Map.of("name", field.getSimpleName())),
-                        ProblemType.FIELD_SHOULD_BE_FINAL);
+                        .filterChildren(ctElement -> ctElement instanceof CtFieldWrite<?> ctFieldWrite
+                            && ctFieldWrite.getVariable().getDeclaration().equals(ctField)
+                            && ctElement.getParent(CtConstructor.class) == null
+                        )
+                        .first() != null;
+
+                boolean hasWriteInConstructor = staticAnalysis.getModel()
+                        .filterChildren(ctElement -> ctElement instanceof CtFieldWrite<?> ctFieldWrite
+                            && ctFieldWrite.getVariable().getDeclaration().equals(ctField)
+                            && ctElement.getParent(CtConstructor.class) != null
+                        )
+                        .first() != null;
+
+                boolean hasExplicitValue = ctField.getDefaultExpression() != null && !ctField.getDefaultExpression().isImplicit();
+
+                if (!hasWrite && !(hasWriteInConstructor && hasExplicitValue)) {
+                    addLocalProblem(
+                        ctField,
+                        new LocalizedMessage("field-final-exp", Map.of("name", ctField.getSimpleName())),
+                        ProblemType.FIELD_SHOULD_BE_FINAL
+                    );
                 }
             }
         });
