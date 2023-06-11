d0149c9aba2bebdefdb71faae90049909b905e3e
Lucas <24826124+Luro02@users.noreply.github.com>
2023-06-11 10:42:06 +0200

    do not count private fields for constant classes #195

--- autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/ConstantsClass/code/Test.java
+++ autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/ConstantsClass/code/Test.java
@@ -1,21 +1,21 @@
 package de.firemage.autograder.core.check_tests.InterfaceBadPractices.code;
 
 public class Test { // Not Ok
-    private static final String A = "";
-    private static final String B = "";
-    private static final String C = "";
-    private static final String D = "";
-    private static final String E = "";
-    private static final String F = "";
-    private static final String G = "";
-    private static final String H = "";
-    private static final String I = "";
-    private static final String J = "";
-    private static final String K = "";
-    private static final String L = "";
-    private static final String M = "";
-    private static final String N = "";
-    private static final String O = "";
+    public static final String A = "";
+    public static final String B = "";
+    public static final String C = "";
+    public static final String D = "";
+    public static final String E = "";
+    public static final String F = "";
+    public static final String G = "";
+    public static final String H = "";
+    public static final String I = "";
+    public static final String J = "";
+    public static final String K = "";
+    public static final String L = "";
+    public static final String M = "";
+    public static final String N = "";
+    public static final String O = "";
 
     public static void main(String[] args) {}
 }
@@ -42,4 +42,20 @@ enum ConstantsEnum { // Not Ok
     ConstantsEnum(String value) {
         this.value = value;
     }
+}
+
+class EverythingPrivate { // Ok
+    private static final String ARGUMENT_NO_AI = "pvp";
+    private static final String ARGUMENT_AI = "ki";
+    private static final int MAX_ARGS_SIZE = 4;
+    private static final int MINIMUM_BOARD_SIZE = 3;
+    private static final int MAXIMUM_BOARD_SIZE = 9;
+    private static final int MINIMUM_WIN_REQUIREMENT = 3;
+    private static final int MAXIMUM_WIN_REQUIREMENT = 9;
+    private static final int WIN_REQUIREMENT_ARGS_INDEX = 2;
+    private static final int MINIMUM_MAX_AMOUNT_ENTRIES = 9;
+    private static final int MAXIMUM_MAX_AMOUNT_ENTRIES = 81;
+    private static final int MAX_AMOUNT_ARGS_INDEX = 3;
+
+    public static void main(String[] args) {}
 }
