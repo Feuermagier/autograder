714310218a121bac7659100c4521064abc250ba1
Lucas <24826124+Luro02@users.noreply.github.com>
2023-06-11 10:56:34 +0200

    ignore regex with comments explaining what they do #193

--- autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/Regex/code/Test.java
+++ autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/Regex/code/Test.java
@@ -1,12 +1,19 @@
 public class Test {
-    private String noRegex = "Should we do this? I guess we shouldn't! f*ck you!"; // ok
-    private String regex1 = "(foo)* [bar]+ x? x?"; // not ok
-    private String regex2 = "(?<g1>foo)"; // not ok
-    private String regex3 = "^[a-z]+(?: \\S+)?$"; // not ok
-    private String regex4 = "^(?<start>\\d+)-->(?<end>\\d+):(?<length>\\d+)m,(?<type>\\d+)x,(?<velocity>\\d+)max$"; // not ok
-    private String regex5 = "^(?<identifier>\\d+),(?<street>\\d+),(?<velocity>\\d+),(?<acceleration>\\d+)$"; // not ok
-    private String simpleRegex1 = "\\d*.\\d*"; // ok
-    private String simpleRegex2 = "\\d*"; // ok
-    private String simpleRegex3 = "^[a-z]+"; // ok
-    private String invalidRegex = "(foo* [bar]+ x? x?"; // ok
+    private String noRegex = "Should we do this? I guess we shouldn't! f*ck you!";
+    private String regex1 = "(foo)* [bar]+ x? x?";
+    private String regex2 = "(?<g1>foo)";
+    private String regex3 = "^[a-z]+(?: \\S+)?$";
+    private String regex4 = "^(?<start>\\d+)-->(?<end>\\d+):(?<length>\\d+)m,(?<type>\\d+)x,(?<velocity>\\d+)max$";
+    private String regex5 = "^(?<identifier>\\d+),(?<street>\\d+),(?<velocity>\\d+),(?<acceleration>\\d+)$";
+    private String simpleRegex1 = "\\d*.\\d*";
+    private String simpleRegex2 = "\\d*";
+    private String simpleRegex3 = "^[a-z]+";
+    private String invalidRegex = "(foo* [bar]+ x? x?";
+
+    /**
+     * This comment is explaining how the regex works...
+     */
+    private static final String COMPLICATED_REGEX_1 = "^(?<identifier>\\d+),(?<street>\\d+),(?<velocity>\\d+),(?<acceleration>\\d+)$";
+    // Inline comments should be acceptable as well
+    private static final String COMPLICATED_REGEX_2 = "^(?<identifier>\\d+),(?<street>\\d+),(?<velocity>\\d+),(?<acceleration>\\d+)$";
 }
