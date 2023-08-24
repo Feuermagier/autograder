6bca3a2a8da1e5afce7852487ed12aa764412cc6
Lucas <24826124+Luro02@users.noreply.github.com>
2023-08-24 14:06:28 +0200

    remove `MathFloorDivision` test folder

--- autograder-core/src/test/resources/de/firemage/autograder/core/check_tests/MathFloorDivision/code/Test.java
+++
@@ -1,9 +0,0 @@
-package de.firemage.autograder.core.check_tests.MathFloorDivision.code;
-
-public class Test {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 2;
-        int c = (int) Math.floor(a / b) + 1; /*# not ok #*/
-    }
-}
