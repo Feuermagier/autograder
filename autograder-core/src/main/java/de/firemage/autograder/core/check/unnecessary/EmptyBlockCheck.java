4c1663d25444decf2a6b6316e875ec7c08c66af5
Luro02 <24826124+Luro02@users.noreply.github.com>
2024-04-08 20:20:53 +0200

    ignore overridden methods for empty block check

--- autograder-core/src/main/java/de/firemage/autograder/core/check/unnecessary/EmptyBlockCheck.java
+++ autograder-core/src/main/java/de/firemage/autograder/core/check/unnecessary/EmptyBlockCheck.java
@@ -11,6 +11,7 @@ import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtCatch;
 import spoon.reflect.code.CtComment;
 import spoon.reflect.code.CtSwitch;
+import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.visitor.CtScanner;
 
 @ExecutableCheck(reportedProblems = {ProblemType.EMPTY_BLOCK, ProblemType.EMPTY_CATCH})
@@ -28,6 +29,12 @@ public class EmptyBlockCheck extends IntegratedCheck {
             public <T> void visitCtBlock(CtBlock<T> ctBlock) {
                 if (ctBlock.isImplicit() || !ctBlock.getPosition().isValidPosition() || !isEmptyBlock(ctBlock)) {
                     super.visitCtBlock(ctBlock);
+                    return;
+                }
+
+                if (ctBlock.getParent() instanceof CtMethod<?> ctMethod
+                    && ctMethod.getBody().equals(ctBlock)
+                    && SpoonUtil.isInOverriddenMethod(ctBlock)) {
                     return;
                 }
 
