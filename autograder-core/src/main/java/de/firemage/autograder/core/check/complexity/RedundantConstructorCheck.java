f8790119b7cf283f9416e94197d7e762870a1b94
F Schwalbe <frederick@fschwal.be>
2024-04-09 12:57:07 +0200

    Rewrite RedundantConstructorCheck without PMD

--- autograder-core/src/main/java/de/firemage/autograder/core/check/complexity/RedundantConstructorCheck.java
+++ autograder-core/src/main/java/de/firemage/autograder/core/check/complexity/RedundantConstructorCheck.java
@@ -2,14 +2,62 @@ package de.firemage.autograder.core.check.complexity;
 
 import de.firemage.autograder.core.LocalizedMessage;
 import de.firemage.autograder.core.ProblemType;
+import de.firemage.autograder.core.Translatable;
 import de.firemage.autograder.core.check.ExecutableCheck;
-import de.firemage.autograder.core.pmd.PMDCheck;
-import net.sourceforge.pmd.lang.java.rule.codestyle.UnnecessaryConstructorRule;
+import de.firemage.autograder.core.dynamic.DynamicAnalysis;
+import de.firemage.autograder.core.integrated.IntegratedCheck;
+import de.firemage.autograder.core.integrated.StaticAnalysis;
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.declaration.CtElement;
+
+import java.util.function.Predicate;
 
 @ExecutableCheck(reportedProblems = {ProblemType.REDUNDANT_DEFAULT_CONSTRUCTOR})
-public class RedundantConstructorCheck extends PMDCheck {
-    public RedundantConstructorCheck() {
-        super(new LocalizedMessage("implicit-constructor-exp"),
-            new UnnecessaryConstructorRule(), ProblemType.REDUNDANT_DEFAULT_CONSTRUCTOR);
+public class RedundantConstructorCheck extends IntegratedCheck {
+    private static final Translatable MESSAGE = new LocalizedMessage("implicit-constructor-exp");
+
+    @Override
+    protected void check(StaticAnalysis staticAnalysis, DynamicAnalysis dynamicAnalysis) {
+        staticAnalysis.processWith(new AbstractProcessor<CtClass<?>>() {
+            @Override
+            public void process(CtClass<?> element) {
+                var ctors = element.getConstructors();
+                if (ctors.size() != 1) return;
+
+                var ctor = ctors.iterator().next();
+                var isRedundant = !ctor.isImplicit()
+                    && ctor.getParameters().isEmpty()
+                    && hasDefaultVisibility(element, ctor)
+                    && isEmpty(ctor.getBody())
+                    && ctor.getThrownTypes().isEmpty();
+                if (isRedundant) {
+                    addLocalProblem(ctor, MESSAGE, ProblemType.REDUNDANT_DEFAULT_CONSTRUCTOR);
+                }
+            }
+        });
+    }
+
+    private boolean hasDefaultVisibility(CtClass<?> type, CtConstructor<?> ctor) {
+        // enum constructors are always private
+        return type.isEnum() || ctor.getVisibility() == type.getVisibility();
+    }
+
+    private boolean isEmpty(CtBlock<?> block) {
+        return block
+            .getStatements()
+            .stream()
+            .filter(Predicate.not(CtElement::isImplicit))
+            // A constructor invocation is either this or super.
+            // Because we know we are analyzing the body of a no-args constructor, it
+            // cannot be a recursive this() call, but has to be a redundant super() call.
+            .filter(statement -> !(statement instanceof CtInvocation<?> invocation
+                && invocation.getExecutable().isConstructor()
+                && invocation.getArguments().isEmpty()))
+            .findAny()
+            .isEmpty();
     }
 }
---
+++ autograder-core/src/test/java/de/firemage/autograder/core/check/complexity/TestRedundantConstructor.java
@@ -0,0 +1,219 @@
+package de.firemage.autograder.core.check.complexity;
+
+import de.firemage.autograder.core.LinterException;
+import de.firemage.autograder.core.LocalizedMessage;
+import de.firemage.autograder.core.Problem;
+import de.firemage.autograder.core.ProblemType;
+import de.firemage.autograder.core.check.AbstractCheckTest;
+import de.firemage.autograder.core.compiler.JavaVersion;
+import de.firemage.autograder.core.file.SourceInfo;
+import de.firemage.autograder.core.file.StringSourceInfo;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestRedundantConstructor extends AbstractCheckTest {
+    private static final List<ProblemType> PROBLEM_TYPES = List.of(ProblemType.REDUNDANT_DEFAULT_CONSTRUCTOR);
+
+    @Test
+    void testRedundantConstructor() throws IOException, LinterException {
+        assertRedundant(
+            "Test",
+            """
+                public class Test {
+                    public Test() {
+                    
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testRedundantConstructorSuper() throws IOException, LinterException {
+        assertRedundant(
+            "Test",
+            """
+                public class Test {
+                    public Test() {
+                        super();
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testRedundantConstructorInnerClassSuper() throws IOException, LinterException {
+        assertRedundant(
+            "Test",
+            """
+                public class Test {
+                    public class Inner {
+                        public Inner() {
+                            super(/* test */);
+                        }
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testRedundantEnumConstructor() throws IOException, LinterException {
+        assertRedundant(
+            "TestEnum",
+            """
+                public enum TestEnum {
+                    ;
+                    private TestEnum() {
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testRedundantEnumConstructorNoModifier() throws IOException, LinterException {
+        assertRedundant(
+            "TestEnum",
+            """
+                public enum TestEnum {
+                    ;
+                    TestEnum() {
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testRedundantRecordCompactConstructor() throws IOException, LinterException {
+        assertRedundant(
+            "Test",
+            """
+                record Test() {
+                    Test {
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testRedundantRecordNormalConstructor() throws IOException, LinterException {
+        assertRedundant(
+            "Test",
+            """
+                public record Test() {
+                    public Test() {
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testNotRedundantImplicitConstructor() throws IOException, LinterException {
+        assertNotRedundant(
+            "Test",
+            """
+                public class Test {
+                }
+                """
+        );
+    }
+
+    @Test
+    void testNotRedundantImplicitRecordConstructor() throws IOException, LinterException {
+        assertNotRedundant(
+            "TestRecord",
+            """
+                record TestRecord() {
+                }
+                """
+        );
+    }
+
+    @Test
+    void testNotRedundantConstructorVisibility() throws IOException, LinterException {
+        assertNotRedundant(
+            "Test",
+            """
+                public class Test {
+                    protected Test() {
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testNotRedundantBody() throws IOException, LinterException {
+        assertNotRedundant(
+            "Test",
+            """
+                public class Test {
+                    public Test() {
+                        System.out.println();
+                    }
+                }
+                """
+        );
+    }
+
+    @Test
+    void testNotRedundantConstructorSuperArgs() throws IOException, LinterException {
+        assertRedundant(
+            StringSourceInfo.fromSourceStrings(JavaVersion.JAVA_17, Map.of(
+                "Base",
+                """
+                    public class Base {
+                        public Base(String foo) {
+                        }
+                    }
+                    """,
+                "Child",
+                """
+                    public class Child extends Base {
+                        public Child() {
+                            super("foo");
+                        }
+                    }
+                    """
+            )),
+            false
+        );
+    }
+
+    private void assertRedundant(String className, String source) throws LinterException, IOException {
+        assertRedundant(className, source, true);
+    }
+
+    private void assertNotRedundant(String className, String source) throws LinterException, IOException {
+        assertRedundant(className, source, false);
+    }
+
+    private void assertRedundant(String className, String source, boolean redundant) throws LinterException, IOException {
+        assertRedundant(StringSourceInfo.fromSourceString(JavaVersion.JAVA_17, className, source), redundant);
+    }
+
+    private void assertRedundant(SourceInfo info, boolean redundant) throws LinterException, IOException {
+        ProblemIterator problems = this.checkIterator(info, PROBLEM_TYPES);
+        if (redundant) {
+            assertEqualsRedundant(problems.next());
+        }
+        problems.assertExhausted();
+    }
+
+    private void assertEqualsRedundant(Problem problem) {
+        assertEquals(
+            this.linter.translateMessage(new LocalizedMessage("implicit-constructor-exp")),
+            this.linter.translateMessage(problem.getExplanation())
+        );
+    }
+}
