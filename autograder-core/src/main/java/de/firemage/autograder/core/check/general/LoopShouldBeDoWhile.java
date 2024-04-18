fb75a1624d1d0ee27f2b9bf375c068e45e58b42b
Luro02 <24826124+Luro02@users.noreply.github.com>
2024-04-18 08:49:44 +0200

    fix merge conflicts

--- autograder-core/src/main/java/de/firemage/autograder/core/check/general/LoopShouldBeDoWhile.java
+++ autograder-core/src/main/java/de/firemage/autograder/core/check/general/LoopShouldBeDoWhile.java
@@ -3,7 +3,6 @@ package de.firemage.autograder.core.check.general;
 import de.firemage.autograder.core.LocalizedMessage;
 import de.firemage.autograder.core.ProblemType;
 import de.firemage.autograder.core.check.ExecutableCheck;
-import de.firemage.autograder.core.dynamic.DynamicAnalysis;
 import de.firemage.autograder.core.integrated.IntegratedCheck;
 import de.firemage.autograder.core.integrated.SpoonUtil;
 import de.firemage.autograder.core.integrated.StaticAnalysis;
@@ -19,7 +18,7 @@ import java.util.Map;
 @ExecutableCheck(reportedProblems = { ProblemType.LOOP_SHOULD_BE_DO_WHILE })
 public class LoopShouldBeDoWhile extends IntegratedCheck {
     @Override
-    protected void check(StaticAnalysis staticAnalysis, DynamicAnalysis dynamicAnalysis) {
+    protected void check(StaticAnalysis staticAnalysis) {
         staticAnalysis.processWith(new AbstractProcessor<CtWhile>() {
             @Override
             public void process(CtWhile ctWhile) {
