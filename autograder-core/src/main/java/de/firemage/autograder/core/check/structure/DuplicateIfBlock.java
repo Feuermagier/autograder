4f95b7261a359db6b88b0737459c0a565ef7c64b
Luro02 <24826124+Luro02@users.noreply.github.com>
2024-12-17 12:26:21 +0100

    rewrite `DuplicateIfBlock` check to detect more things

--- autograder-core/src/main/java/de/firemage/autograder/core/check/structure/DuplicateIfBlock.java
+++ autograder-core/src/main/java/de/firemage/autograder/core/check/structure/DuplicateIfBlock.java
@@ -12,39 +12,117 @@ import spoon.reflect.code.CtIf;
 import spoon.reflect.code.CtStatement;
 import spoon.reflect.declaration.CtElement;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @ExecutableCheck(reportedProblems = { ProblemType.DUPLICATE_IF_BLOCK })
 public class DuplicateIfBlock extends IntegratedCheck {
-    private static List<CtIf> getDuplicates(CtIf ctIf, Predicate<? super CtStatement> isDuplicate) {
-        List<CtIf> result = new ArrayList<>();
-
-        List<CtStatement> followingStatements;
+    private static List<CtStatement> getElseStatements(CtIf ctIf, List<CtStatement> followingStatements) {
+        List<CtStatement> elseStatements;
         if (ctIf.getElseStatement() == null) {
-            // the if does not have an else, so check if the following statements are duplicate if blocks
-            followingStatements = StatementUtil.getNextStatements(ctIf);
+            // check if the if block is terminal, because when it is not, we cannot merge it with the following statements
+            // with the if block, because they are executed after the if block has been executed.
+            //
+            // if (condition) {
+            //      System.out.println("a");
+            // }
+            //
+            // System.out.println("a");
+            //
+            // if (otherCondition) {
+            //      System.out.println("a");
+            // }
+            if (ctIf.getThenStatement() == null || !StatementUtil.isTerminal(ctIf.getThenStatement())) {
+                return null;
+            }
 
-
+            elseStatements = followingStatements;
         } else {
-            // the if has an else, so check if the else has an if with a duplicate block
-            followingStatements = StatementUtil.getEffectiveStatements(ctIf.getElseStatement());
+            // there is an explicit else block
+            List<CtStatement> statements = StatementUtil.getEffectiveStatements(ctIf.getElseStatement());
+            // if the then block is not terminal, we cannot merge the else block if there is more than an if statement in it
+            //
+            // if (condition) {
+            //    System.out.println("a");
+            // } else {
+            //     if (otherCondition) {
+            //         System.out.println("a");
+            //     }
+            //
+            //     System.out.println("b");
+            // }
+            if (ctIf.getThenStatement() != null && !StatementUtil.isTerminal(ctIf.getThenStatement()) && statements.size() > 1) {
+                return null;
+            }
+
+            elseStatements = new ArrayList<>(statements);
+            // if the then block is terminal, we can merge the else block with the following statements
+            if (ctIf.getThenStatement() != null && StatementUtil.isTerminal(ctIf.getThenStatement())) {
+                elseStatements.addAll(followingStatements);
+            }
         }
 
-        for (var statement : followingStatements) {
+        return elseStatements;
+    }
+
+    // This function extracts duplicates from the if/else-if/else blocks that could be merged into one if block.
+    private static List<CtIf> listDuplicates(CtIf ctIf, Predicate<? super CtStatement> isDuplicate) {
+        List<CtIf> result = new ArrayList<>();
+
+        List<CtStatement> elseStatements = getElseStatements(ctIf, StatementUtil.getNextStatements(ctIf));
+        if (elseStatements == null) {
+            return result;
+        }
+
+        Queue<CtStatement> queue = new ArrayDeque<>(elseStatements);
+        while (!queue.isEmpty()) {
+            CtStatement statement = queue.poll();
+
+            // these are the conditions for merging the previous if blocks with the current one:
             if (!(statement instanceof CtIf nextIf) || nextIf.getThenStatement() == null || !isDuplicate.test(nextIf.getThenStatement())) {
                 break;
             }
 
-            if (nextIf.getElseStatement() == null) {
+            // Now we need to check the else block, for example in the following code one can not merge the if blocks:
+            //
+            // if (i <= 0) {
+            //     System.out.println("a");
+            // } else {
+            //     if (i + i > 1) {
+            //         System.out.println("a");
+            //     } else {
+            //         System.out.println("c");
+            //     }
+            // }
+
+            // if there is no else block, we can merge the if blocks
+            if (nextIf.getElseStatement() == null && StatementUtil.isTerminal(nextIf.getThenStatement())) {
                 result.add(nextIf);
+                continue;
             }
+
+            // Otherwise call the function which adjusts the else statements for possible merging
+            List<CtStatement> nextElseStatements = getElseStatements(nextIf, new ArrayList<>(queue));
+            if (nextElseStatements == null) {
+                break;
+            }
+
+            // the else block seems to be mergeable:
+            if (nextElseStatements.isEmpty()) {
+                result.add(nextIf);
+                break;
+            }
+
+            result.add(nextIf);
+            queue = new ArrayDeque<>(nextElseStatements);
         }
 
         return result;
@@ -60,16 +138,14 @@ public class DuplicateIfBlock extends IntegratedCheck {
                     return;
                 }
 
-                // what we check:
-                // - if (condition) followed by another if (condition)
-                // - if (condition) with an else { if (condition) }
+                List<CtIf> duplicates = listDuplicates(
+                    ctIf,
+                    ctStatement -> DuplicateCatchBlock.isDuplicateBlock(ctIf.getThenStatement(), ctStatement, difference -> false)
+                );
 
-                // for the merging to work, the if block must be terminal (i.e. ends with return, throw, break, continue)
-                if (ctIf.getThenStatement() == null || !StatementUtil.isTerminal(ctIf.getThenStatement())) {
+                if (duplicates.stream().anyMatch(visited::contains)) {
                     return;
                 }
-
-                List<CtIf> duplicates = getDuplicates(ctIf, ctStatement -> DuplicateCatchBlock.isDuplicateBlock(ctIf.getThenStatement(), ctStatement, difference -> false));
 
                 if (!duplicates.isEmpty()) {
                     visited.add(ctIf);
