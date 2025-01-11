592eddb47c295f0028b3313ca231a54e15c8deb0
Florian (Feuermagier) <floseligmann@web.de>
2025-01-11 12:10:42 +0100

    wrap messages from errorprone & pmd; check for nested localizedmessageforproblems

--- autograder-core/src/main/java/de/firemage/autograder/core/LocalizedMessageForProblem.java
+++ autograder-core/src/main/java/de/firemage/autograder/core/LocalizedMessageForProblem.java
@@ -7,6 +7,12 @@ import fluent.bundle.FluentBundle;
 import java.util.Optional;
 
 public record LocalizedMessageForProblem(Translatable translatable, ProblemType problemType) implements Translatable {
+    public LocalizedMessageForProblem {
+        if (translatable instanceof LocalizedMessageForProblem) {
+            throw new IllegalArgumentException("LocalizedMessageForProblem cannot be nested");
+        }
+    }
+
     @Override
     public String format(AbstractTranslations translations) {
         var conditionalBundle = translations.getConditionalTranslations(problemType);
--- autograder-extra/src/main/java/de/firemage/autograder/extra/pmd/PMDInCodeProblem.java
+++ autograder-extra/src/main/java/de/firemage/autograder/extra/pmd/PMDInCodeProblem.java
@@ -1,6 +1,7 @@
 package de.firemage.autograder.extra.pmd;
 
 import de.firemage.autograder.core.CodePosition;
+import de.firemage.autograder.core.LocalizedMessageForProblem;
 import de.firemage.autograder.core.Problem;
 import de.firemage.autograder.core.LocalizedMessage;
 import de.firemage.autograder.core.file.SourceInfo;
@@ -12,24 +13,26 @@ public class PMDInCodeProblem extends Problem {
 
     public PMDInCodeProblem(PMDCheck check, RuleViolation violation, SourceInfo sourceInfo) {
         super(check,
-            new CodePosition(
-                sourceInfo,
-                sourceInfo.getCompilationUnit(Path.of(violation.getFileId().getOriginalPath())).path(),
-                violation.getBeginLine(),
-                violation.getBeginLine(),
-                violation.getBeginColumn(),
-                violation.getBeginColumn()),
-            check.getExplanation() != null ? check.getExplanation().apply(violation) : new LocalizedMessage(violation.getDescription()),
-            check.getProblemType());
+                new CodePosition(
+                        sourceInfo,
+                        sourceInfo.getCompilationUnit(Path.of(violation.getFileId().getOriginalPath())).path(),
+                        violation.getBeginLine(),
+                        violation.getBeginLine(),
+                        violation.getBeginColumn(),
+                        violation.getBeginColumn()),
+                new LocalizedMessageForProblem(
+                        check.getExplanation() != null ? check.getExplanation().apply(violation) : new LocalizedMessage(violation.getDescription()),
+                        check.getProblemType()),
+                check.getProblemType());
     }
 
     @Override
     public String toString() {
         return "PMDInCodeProblem[check=%s, position=%s, explanation=%s, problemType=%s]".formatted(
-            this.getCheck(),
-            this.getPosition(),
-            this.getExplanation(),
-            this.getProblemType()
+                this.getCheck(),
+                this.getPosition(),
+                this.getExplanation(),
+                this.getProblemType()
         );
     }
 }
